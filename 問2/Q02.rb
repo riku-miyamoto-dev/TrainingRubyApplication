3dcb2aec17818f5cad730792b182c1c9d429d269
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-10-20 12:03:07 +0900

    #3 コードを必要最小限に変更

--- 問2/Q02.rb
+++ 問2/Q02.rb
@@ -18,24 +18,11 @@ RENAMES = {
 
 # クラス定義
 class PersonalInfo
-  attr_reader:name
+  # ゲッターというインスタンス変数をクラス定義外でも呼び出せるようにするものを設定
+  attr_reader :name
 
   def initialize(row)
-    @id = row["id"]
     @name = row["name"]
-    @ruby = row["rubi"]
-    @sex = row["sex"]
-    @tel = row["tel"]
-    @mobile = row["mobile "]
-    @mail = row["mail"]
-    @zip = row["zip"]
-    @address1 = row["address1"]
-    @address2 = row["address2"]
-    @address3 = row["address3"]
-    @address4 = row["address4"]
-    @address5 = row["address5"]
-    @birthday = row["birthday"]
-
   end
 end
 
@@ -45,7 +32,7 @@ csv_data = File.read(File.join(__dir__, 'personal_infomation.csv'))
 #parseメソッドを使ってパースを行う。その際にリネームを行う
 personal_info_data = CSV.parse(csv_data, headers:true, header_converters: lambda{|h| RENAMES[h]})
 
-#mapメソッドを用いて配列にパースしたデータ構造をハッシュに変換して保存
+#mapメソッドを用いて配列にパースしたデータ構造をクラスで宣言する
 personal_info_array = personal_info_data.map{|row| PersonalInfo.new(row)}
 
 
@@ -53,9 +40,3 @@ personal_info_array = personal_info_data.map{|row| PersonalInfo.new(row)}
 personal_info_array.each do |personal_info|
   puts personal_info.name
 end 
-
-# 確認用
-# p personal_info_data.class
-# p personal_info_array.class
-# p personal_info_array.first.class
-# puts personal_info_array[2].name
