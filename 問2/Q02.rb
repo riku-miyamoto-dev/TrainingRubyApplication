fbe691649d9d6fedfec236fa55e9df8c9066bc7c
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-11-11 09:15:33 +0900

    #07 フィードバック修正

--- 問2/Q02.rb
+++ 問2/Q02.rb
@@ -19,24 +19,24 @@ RENAMES = {
 }
 
 
-#ファイルパスを取得し、csvファイルを読み込む
+# ファイルパスを取得し、csvファイルを読み込む
 file_data = File.read(File.join(__dir__,'..', 'personal_infomation.csv'))
 
-#parseメソッドを使ってパースを行う。
+# parseメソッドを使ってパースを行う。
 csv_data = CSV.parse(file_data, headers:true,)
 
-#mapメソッドを用いて配列にパースしたデータ構造をクラスで宣言する。csv_data.class => CSV::Tableなのでmapメソッドで１行ずつ分解され処理を行う。
+# mapメソッドを用いて配列にパースしたデータ構造をクラスで宣言する。csv_data.class => CSV::Tableなのでmapメソッドで１行ずつ分解され処理を行う。
 # rowの中身は<CSV::Row "no":"5000" "namae":"浜田 次男" "rubi":"ハマダ ツギオ" "seibetu":"男" "denwa":"019-880-9645" "keitai":nil "mairu":"Tsugio_Hamada@knrre.uihln.og" "yuubinbango":"028-0143" "jusho1":"岩手県" "jusho2":"花巻市" "jusho3":"東和町上浮田" "jusho4":"1-1-18" "jusho5":nil "tanjobi":"1941/11/23">
-personal_info_ary = csv_data.map do |row|
+ users= csv_data.map do |row|
   renames_row = row.to_h.transform_keys {|h|RENAMES[h]}
 # クラスのインスタンスを定義
-#renames_rowの中身{"id" => "5000", "name" => "浜田 次男", "ruby" => "ハマダ ツギオ", "sex" => "男", "tel" => "019-880-9645", "mobile" => nil, "mail" => "Tsugio_Hamada@knrre.uihln.og", "zip" => "028-0143", "address1" => "岩手県", "address2" => "花巻市", "address3" => "東和町上浮田", "address4" => "1-1-18", "address5" => nil, "birthday" => "1941/11/23"}
+# renames_rowの中身{"id" => "5000", "name" => "浜田 次男", "ruby" => "ハマダ ツギオ", "sex" => "男", "tel" => "019-880-9645", "mobile" => nil, "mail" => "Tsugio_Hamada@knrre.uihln.og", "zip" => "028-0143", "address1" => "岩手県", "address2" => "花巻市", "address3" => "東和町上浮田", "address4" => "1-1-18", "address5" => nil, "birthday" => "1941/11/23"}
   PersonalInfo.new(renames_row)
-#インスタンスの中身 <PersonalInfo:0x000000011cb4d6c0 @name="浜田 次男", @address1="岩手県", @address2="花巻市", @address3="東和町上浮田", @address4="1-1-18", @address5=nil, @birthday="1941/11/23">
+# インスタンスの中身 <PersonalInfo:0x000000011cb4d6c0 @name="浜田 次男", @address1="岩手県", @address2="花巻市", @address3="東和町上浮田", @address4="1-1-18", @address5=nil, @birthday="1941/11/23">
 end
 
-# # each do で配列に対して名前のみを取り出す処理を行う
-personal_info_ary.each do |user|
+# each do で配列に対して名前のみを取り出す処理を行う
+users.each do |user|
   puts user.name
   puts user.address
   puts user.age
--- 問2/personal_info.rb
+++ 問2/personal_info.rb
@@ -14,19 +14,17 @@ class PersonalInfo
   end
 
   def address
-    # アドレスのみの配列を作成
-    address_ary = [@address1, @address2, @address3, @address4, @address5]
     # 配列からcompactメソッドでnilを削除し、joinメソッドで配列を連結し文字列に返す
-    address_ary.compact.join
+    [@address1, @address2, @address3, @address4, @address5].compact.join
   end
 
   def age
     # 年齢を計算するための現在時刻を取得
-    now_time = Time.now
+    today = Date.today
     # Time-Timeにして計算を可能にするため string => Time 変換をparseメソッドで行う 
-    birth = Time.parse(@birthday)
-    # 計算結果が秒数で出力されるため、年間の秒数で割る。年齢を求める際、小数点以下は不要なのでfloorメソッドで切り捨て
-    age = ((now_time - birth)/(60*60*24*365)).floor(0)
+    birthday = Date.parse(@birthday)
+    # 計算結果が日数で出力されるため、年間の日数で割る。年齢を求める際、小数点以下は不要なのでfloorメソッドで切り捨て
+    age = ((today - birthday)/365).floor(0)
     age
   end
 end
