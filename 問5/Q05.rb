6415f415b31888b14d89747bad33b5d4e7cb431d
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-12-02 10:03:12 +0900

    #13 課題解答

---
+++ 問5/Q05.rb
@@ -0,0 +1,38 @@
+require "erb"
+require "csv"
+require "./personal_info"
+
+RENAMES = {
+  'no' => 'id',
+  'namae' => 'name',
+  'rubi' => 'ruby',
+  'seibetu' => 'sex',
+  'denwa' => 'tel',  
+  'keitai' => 'mobile',
+  'mairu' => 'mail',
+  'yuubinbango' => 'zip',
+  'jusho1' => 'address1',
+  'jusho2' => 'address2',
+  'jusho3' => 'address3',
+  'jusho4' => 'address4',
+  'jusho5' => 'address5',
+  'tanjobi' => 'birthday',
+}
+
+# csvファイルを読み込みCSVクラスのインスタンスを作成
+file_data = File.read(File.join(__dir__,'..', 'personal_infomation.csv'))
+csv_data = CSV.parse(file_data, headers:true,)
+
+# ヘッダー命名変更し、PersonalInfoクラスのインスタンスを作成
+users = csv_data.map do |row|
+  renames_row = row.to_h.transform_keys do |h|
+    RENAMES[h]
+  end
+  PersonalInfo.new(renames_row)
+end
+
+
+# eRubyスクリプトからerbオブジェクトを生成し、HTMLの作成
+erb_data = ERB.new(File.read("table.erb"))
+File.write("table.html", erb_data.result)
+puts erb_data.result
---
+++ 問5/personal_info.rb
@@ -0,0 +1,25 @@
+# クラス定義
+class PersonalInfo
+  #メソッドでスコープ外でも使う変数を定義
+  attr_accessor :id, :name, :address1, :address2, :address3, :address4, :address5, :birthday
+
+  def initialize(row)
+    @id = row ["id"]
+    @name = row["name"]
+    @address1 = row["address1"]
+    @address2 = row["address2"]
+    @address3 = row["address3"]
+    @address4 = row["address4"]
+    @address5 = row["address5"]
+    @birthday = row["birthday"]
+  end
+
+  def address
+    [@address1, @address2, @address3, @address4, @address5].compact.join
+  end
+
+  def age
+    # Time-Timeで日数計算を行う。今日の日付から生年月日を引き、年間の日数で割る。その際小数点以下は不要なのでfloorメソッドで切り捨てる。@birthdayはTimeクラスではないためparseで変換
+    age = ((Date.today- Date.parse(@birthday))/365).floor(0)
+  end
+end
