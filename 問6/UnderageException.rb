f0c031e57518f15e9bfc2af82228c28c326e2419
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-12-08 13:57:08 +0900

    #15 レビュー指摘箇所(Class内で例外クラスの作成, beginの導入)

--- 問6/UnderageException.rb
+++
@@ -1,6 +0,0 @@
-#エラー名を出すためにStandardErrorを継承
-class UnderageException < StandardError
-  def initialize
-    puts "18歳未満です"
-  end
-end
--- 問6/User.rb
+++ 問6/User.rb
@@ -1,13 +1,19 @@
-require "./UnderageException"
 class User
+  #エラー名を出すためにStandardErrorを継承
+  class UnderageException < StandardError
+  end
+
   # クラス生成時年齢を判断するため
   def initialize(age:, name:)
-    # 課題指定の年齢
-    if age < 18
-      # raiseは例外オブジェクトを受け取るためクラスを参照
-      raise UnderageException
-    else
+    begin
+      # 課題指定の年齢
+      if age < 18
+        # raiseは例外オブジェクトを受け取るためクラスを参照
+        raise UnderageException
+      end
       puts "インスタンスの作成に成功 => #{name}さん#{age}歳"
+    rescue
+      puts "18歳未満のためエラー発生"
     end
   end
 end
