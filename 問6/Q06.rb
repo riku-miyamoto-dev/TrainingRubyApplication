b45482f3599fe96f25455a52af34dd8760c46445
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-12-08 14:30:18 +0900

    #15 beginの挿入箇所変更

--- 問6/Q06.rb
+++ 問6/Q06.rb
@@ -1,7 +1,10 @@
 require "./User"
-
-User.new(age:42, name: "伊藤")
-User.new(name: "田中", age:25)
-User.new(name: "佐藤", age:18)
-User.new(age:38, name: "谷崎")
-User.new(name: "武田", age:17)
+begin  
+  User.new(age:42, name: "伊藤")  
+  User.new(name: "田中", age:25)
+  User.new(name: "佐藤", age:18)
+  User.new(age:38, name: "谷崎")
+  User.new(name: "武田", age:17)
+rescue 
+  puts "18歳未満のためエラー発生"
+end
--- 問6/User.rb
+++ 問6/User.rb
@@ -5,15 +5,11 @@ class User
 
   # クラス生成時年齢を判断するため
   def initialize(age:, name:)
-    begin
       # 課題指定の年齢
       if age < 18
         # raiseは例外オブジェクトを受け取るためクラスを参照
         raise UnderageException
       end
       puts "インスタンスの作成に成功 => #{name}さん#{age}歳"
-    rescue
-      puts "18歳未満のためエラー発生"
-    end
   end
 end
