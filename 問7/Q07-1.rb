5adcb93f27eb2c302ce80b0ed8085101266b332a
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-12-09 12:27:36 +0900

    #17  URLをFile.join結合

--- 問7/Q07-1.rb
+++ 問7/Q07-1.rb
@@ -1,9 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html_document = Nokogiri::HTML5(URI.open('https://www.kurobe-dam.com/'))
+base_url = 'https://www.kurobe-dam.com/'
+html_document = Nokogiri::HTML5(URI.open(base_url))
 
 html_document.css('img[src$=".png"]').each do |html_element|
   # 取得できるURLがhttps://www.kurobe-dam.comからの相対パスであるため、全文出力するようにhttps://www.kurobe-dam.comを追加
-  puts "https://www.kurobe-dam.com#{html_element["src"]}"
+  puts File.join(base_url,html_element["src"])
 end
