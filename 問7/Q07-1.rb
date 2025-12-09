fb670a0874472efc5c7181943a10c30bb345768a
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2025-12-09 10:41:19 +0900

    #17 コメント追加、変数名変更

--- 問7/Q07-1.rb
+++ 問7/Q07-1.rb
@@ -1,10 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-# Fetch and parse HTML document
-doc = Nokogiri::HTML5(URI.open('https://www.kurobe-dam.com/'))
+html_document = Nokogiri::HTML5(URI.open('https://www.kurobe-dam.com/'))
 
-
-doc.css('img[src$=".png"]').each do |link|
-  puts "https://www.kurobe-dam.com#{link["src"]}"
+html_document.css('img[src$=".png"]').each do |html_element|
+  # 取得できるURLがhttps://www.kurobe-dam.comからの相対パスであるため、全文出力するようにhttps://www.kurobe-dam.comを追加
+  puts "https://www.kurobe-dam.com#{html_element["src"]}"
 end
